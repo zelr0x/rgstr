07fd33d8151234b1b12afa2ebfd6dbe9ece4e5bc
zelr0x <zelr0xdev@gmail.com>
2021-04-21 04:23:53 +0400

    Added basic figwheel config

--- project.clj
+++ project.clj
@@ -3,8 +3,15 @@
   :url "http://github.com/zelr0x/rgstr"
   :min-lein-version "2.0.0"
   :jvm-opts ["-server"]
+
   :source-paths ["src/clj" "src/cljs"]
+  :resource-paths ["resources"]
+  :target-path "target/%s/"
   :test-paths ["test/clj" "test/cljs"]
+  :clean-targets ^{:protect false} [:target-path
+                                    [:cljsbuild :builds :app :compiler :output-dir]
+                                    [:cljsbuild :builds :app :compiler :output-to]]
+
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.844"]
                  [compojure "1.6.1"]
@@ -13,29 +20,36 @@
                  [ring/ring-json "0.5.1"]
                  [metosin/ring-http-response "0.9.2"]
                  [re-frame "1.2.0"]]
+
   :plugins [[lein-ring "0.12.5"]
-            [lein-cljsbuild "1.1.8"]]
+            [lein-cljsbuild "1.1.8"]
+            [lein-figwheel "0.5.20"]]
+
   :main rgstr.core
   :ring {:handler rgstr.handler/app :port 8080}
-  :cljsbuild {:builds [{:source-paths ["src/cljs"]
-                        :compiler 
-                        {:output-to "resources/public/rgstr.js"
-                         :optimizations :whitespace
-                         :pretty-print true}}]}
+  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
+                             :figwheel true
+                             :compiler
+                             {:output-to "resources/public/js/rgstr.js"
+                              :output-dir "resources/public/js/out"
+                              :asset-path "js/out"
+                              :main rgstr.core
+                              :pretty-print true}}}}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]
                         [cheshire "5.10.0"]]}
+
    :uberjar {:omit-source true
              :uberjar-name "testapp.jar" ;; jar name is a task requirement
              :prep-tasks ["clean" "compile" ["cljsbuild" "once"]]
              :aot :all
              :hooks [leiningen.cljsbuild]
-             :cljsbuild {:jar true
-                         :builds
-                         [{:source-paths ["src/cljs"]
-                           :compiler 
-                           {:optimizations :advanced
-                              :pretty-print false
-                              :closure-warnings {:externs-validation :off
-                                                 :non-standard-jsdoc :off}}}]}}})
+             :cljsbuild {:app {:jar true
+                               :builds
+                               [{:source-paths ["src/cljs"]
+                                 :compiler
+                                 {:optimizations :advanced
+                                  :pretty-print false
+                                  :closure-warnings {:externs-validation :off
+                                                     :non-standard-jsdoc :off}}}]}}}})
