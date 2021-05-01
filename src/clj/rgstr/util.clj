d7c57c0f13057cd4ad698f3c0e0fac7d0ccaa498
zelr0x <zelr0xdev@gmail.com>
2021-05-01 14:27:09 +0400

    Cleaned up util.clj

--- src/clj/rgstr/util.clj
+++ src/clj/rgstr/util.clj
@@ -1,14 +1,6 @@
 (ns rgstr.util
   (:require [tick.alpha.api :as t]))
 
-(defn try-isodate->ld [cs]
-  "Parses the given date CharSequence using ISO 8601."
-  (try
-    (let [parsed (t/parse cs)]
-      {:result parsed})
-    (catch java.lang.IllegalArgumentException e
-      {:error e})))
-
 (defn ld->ldt [ld]
   "Transforms the given LocalDate to LocalDateTime
    using the time of midnight."
@@ -29,22 +21,3 @@
 (defmulti to-date class)
 (defmethod to-date java.util.Date [d] d)
 (defmethod to-date java.time.LocalDate [ld] (ld->date ld))
-
-;; FIXME: this is kinda major bad thing...
-;; I'm not sure how and where use and destructure namespaced (using /) 
-;; keywords properly. All solutions I tried don't seem to work, so I used this.
-;; https://stackoverflow.com/a/44220595
-(defn- transform-keywords [m]
-  (into {}
-        (map (fn [[k v]]
-               (let [k (if (keyword? k) (keyword (name k)) k)
-                     v (if (keyword? v) (name v) v)]
-                 [k v]))
-             m)))
-(defn strip-ns [m]
-  (clojure.walk/postwalk
-   (fn [x]
-     (if (map? x)
-       (transform-keywords x)
-       x))
-   m))
